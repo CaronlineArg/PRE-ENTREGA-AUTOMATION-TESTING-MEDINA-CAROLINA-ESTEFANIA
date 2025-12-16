5db937e78771e984165ee4ad3cd136a061fcc3d4
Carolina Estefanía Medina <33405220+CaronlineArg@users.noreply.github.com>
2025-12-15 22:05:39 -0300

    Prueba de reporte generado en github con la vercion 4 del artifcat

--- tests/ui/test_falla_para_reporte.py
+++ tests/ui/test_falla_para_reporte.py
@@ -3,10 +3,11 @@ from utils.logger import logger
 
 logger.info("Mensaje de prueba")
 
-@pytest.mark.skip(reason="Prueba de fallo comentada para CI")
+@pytest.mark.framework
 def test_falla_ui(driver):
     driver.get("https://www.saucedemo.com/")
     assert False, "Falla intencional para probar screenshot en reporte"
+
 
 
 
