3b2ab267202b2e12524461cd4331ef7cad51edf9
Carolina Estefanía Medina <33405220+CaronlineArg@users.noreply.github.com>
2025-12-15 22:58:49 -0300

    Skip de prueba fallida

--- tests/ui/test_falla_para_reporte.py
+++ tests/ui/test_falla_para_reporte.py
@@ -3,11 +3,10 @@ from utils.logger import logger
 
 logger.info("Mensaje de prueba")
 
-@pytest.mark.framework
+@pytest.mark.skip(reason="Prueba de fallo comentada para CI")
 def test_falla_ui(driver):
     driver.get("https://www.saucedemo.com/")
     assert False, "Falla intencional para probar screenshot en reporte"
-
 
 
 
